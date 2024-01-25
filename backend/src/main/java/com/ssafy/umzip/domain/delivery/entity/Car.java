c9ed65da3dc3f392fea1d6e6189da2408a532d71
ymiiin <102347857+ymkim0216@users.noreply.github.com>
2024-01-25 10:23:45 +0900

    Feat: 용달 계산기 api 개발
    Kakao Mobility API 연결해서 거리를 가져온 후,
    유가정보 API를 이용해서 해당 정보와 알맞게 여러 정보를 조합해서 계산한다.
    ( 유가정보 API 허가 못받음 -> Refactor 필요 )
    
    Related to : #3

--- backend/src/main/java/com/ssafy/umzip/domain/delivery/entity/Car.java
+++ backend/src/main/java/com/ssafy/umzip/domain/delivery/entity/Car.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,16 +24,8 @@ public class Car {
     private String fuel;
     @Column(name = "car_type")
     private int type;
-    /*
-        생각해보니까 차는 create할일이 없음 -> 주석
-     */
-//    @Builder
-//    public Car(Long id, String name, Long price, String description, String fuel, int type) {
-//        this.id = id;
-//        this.name = name;
-//        this.price = price;
-//        this.description = description;
-//        this.fuel = fuel;
-//        this.type = type;
-//    }
+    @Column(name = "car_mileage")
+    private double mileage;
+
+
 }
