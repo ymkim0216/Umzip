212c41b6a445b107123bd6571ea0e9bbf47a3cac
최재성 <we0620@naver.com>
2024-01-29 10:35:29 +0900

    Bug: 대소문자오류수정

--- frontend/src/components/Service/Delivery/DeliveryForm.jsx
+++ frontend/src/components/Service/Delivery/DeliveryForm.jsx
@@ -8,7 +8,7 @@ import Car from "./Car";
 import { Link, useNavigate } from "react-router-dom";
 import CheckButton from "./Check_Button";
 import PhotoView from "./PhotoView";
-import ProgressBar from "./ProgressBar";
+import ProgressBar from "./Progressbar"; // 대소문자 이슈
 const DUMMY_DATA = [
   { name: "다마스", car_description: "어쩌구 저쩌구" },
   { name: "핸들이 고장난 8톤트럭", car_description: " 어쩌구 저쩌구어쩌구 저쩌구" },
