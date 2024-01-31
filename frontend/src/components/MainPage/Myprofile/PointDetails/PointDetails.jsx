2e7f1e31497710069195c040dc0cd28f8c67ea6d
unknown <chiru7080@daum.net>
2024-01-31 12:09:57 +0900

    feat: pointHistory 페이지네이션

--- frontend/src/components/MainPage/Myprofile/PointDetails/PointDetails.jsx
+++ frontend/src/components/MainPage/Myprofile/PointDetails/PointDetails.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import style from './PointDetails.module.css';
 
@@ -15,8 +16,74 @@ function PointDetails() {
             "content" : "용달 이용 적립 ```",
             "how" : "+300"
         },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
+        {
+            "useDate" : "2024.01.16",
+            "content" : "용달 이용 적립 ```",
+            "how" : "+300"
+        },
   ]
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+
+  // 현재 페이지에 맞는 게시물을 계산
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = points.slice(indexOfFirstItem, indexOfLastItem);
+
+  // 페이지 번호를 계산
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(points.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  // 페이지 번호를 클릭했을 때 실행될 함수
+  const paginate = (pageNumber) => {
+    console.log(`Current page before update: ${currentPage}`);
+    setCurrentPage(pageNumber);
+    console.log(`Current page after update: ${pageNumber}`);
+  }
     return (
+        
       <>
       <div className={style.pointsContainer}>
         <h4>map함수 돌리기 포인트내역</h4>
@@ -26,7 +93,7 @@ function PointDetails() {
         <span className={style.headPoint}>포인트</span>
       </div>
         <ListGroup>
-        {points.map((points, index) => (
+        {currentItems.map((points, index) => (
             <ListGroup.Item className={style.listContainer} key={index}>
                 <span className={style.headDate}>{points.useDate}</span>
                 <span className={style.headContent}>{points.content}</span>
@@ -34,6 +101,17 @@ function PointDetails() {
             </ListGroup.Item>
         ))}
         </ListGroup>
+        <nav>
+        <ul className={style.pagination}>
+          {pageNumbers.map(number => (
+            <li key={number} className={style.pageItem}>
+              <button type="button" onClick={() => paginate(number)} className={style.pageLink}>
+                {number}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
         </div>
       </>
     );
