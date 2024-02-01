0fb4e08548aadc74696a2e7c6da08677dfb46e2e
김동현 <smdong972000@naver.com>
2024-02-01 15:41:04 +0900

    Feat. BoardTradeActive Repository 추가
    Related-to: #29

---
+++ backend/src/main/java/com/ssafy/umzip/domain/trade/repository/BoardTradeActiveRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.umzip.domain.trade.repository;
+
+import com.ssafy.umzip.domain.trade.entity.BoardTradeActive;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardTradeActiveRepository extends JpaRepository<BoardTradeActive, Long> {
+}
