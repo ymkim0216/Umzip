7f0df909d7a048aeafadc74fcf6ae53a4b3ad94f
김동현 <smdong972000@naver.com>
2024-02-01 15:36:06 +0900

    Feat. BoardTradeActive Entity 추가
    Related-to: #29

---
+++ backend/src/main/java/com/ssafy/umzip/domain/trade/entity/BoardTradeActive.java
@@ -0,0 +1,38 @@
+package com.ssafy.umzip.domain.trade.entity;
+
+import com.ssafy.umzip.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@ToString
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="board_trade_active")
+public class BoardTradeActive {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="board_trade_active_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="board_trade_id")
+    private BoardTrade boardTrade;
+
+    @OneToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    @Column(name = "board_trade_is_active")
+    private Boolean isActive;
+
+    @Builder
+    public BoardTradeActive(Long id, BoardTrade boardTrade, Member member,
+                            Boolean isActive) {
+        this.id = id;
+        this.boardTrade = boardTrade;
+        this.member = member;
+        this.isActive = isActive;
+    }
+}
