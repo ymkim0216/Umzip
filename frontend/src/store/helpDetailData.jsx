3fcd3fea1321485f69f03bfc5fc78b0cd0f435cf
unknown <chiru7080@daum.net>
2024-02-02 17:58:46 +0900

    Feat: HelpWrite 글전송 완료, 사진전송 오류수정하기

--- frontend/src/store/helpDetailData.jsx
+++ frontend/src/store/helpDetailData.jsx
@@ -7,7 +7,7 @@ const useStore = create((set, get) => ({
   data: [],
   loading: false,
   error: null,
-  token: 'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QxMjM0Iiwicm9sZSI6IlVTRVIiLCJpZCI6NCwic2lndW5ndSI6MTAwLCJpYXQiOjE3MDY4MzY2MTUsImV4cCI6MTcwNzI2ODYxNX0.5jG0Us4yKDSTowVlebRwW1iP03py78fdSdPCcRTzKnk',
+  token: 'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QxMjM0Iiwicm9sZSI6IlVTRVIiLCJpZCI6NCwic2lndW5ndSI6MTAwLCJpYXQiOjE3MDY4NTc1NTcsImV4cCI6MTcwNzI4OTU1N30.vNY7bIMHN_r40vZh_TF_TxKFpy6M48VA7mJn_7F79L4',
 
   // 매번 다르게 요청하기위한 변수 설정
   boardId: 0, // 검색 키워드 상태
@@ -20,7 +20,7 @@ const useStore = create((set, get) => ({
         // 도메인주소로 할시에는 https로 바꿔줘야함
       const response = await axios.get(`http://192.168.30.206:8080/api/helps/detail/${boardId}`,
       {headers: {
-        Authorization:`Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QxMjM0Iiwicm9sZSI6IlVTRVIiLCJpZCI6NCwic2lndW5ndSI6MTAwLCJpYXQiOjE3MDY4MzY2MTUsImV4cCI6MTcwNzI2ODYxNX0.5jG0Us4yKDSTowVlebRwW1iP03py78fdSdPCcRTzKnk`
+        Authorization:`Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QxMjM0Iiwicm9sZSI6IlVTRVIiLCJpZCI6NCwic2lndW5ndSI6MTAwLCJpYXQiOjE3MDY4NTc1NTcsImV4cCI6MTcwNzI4OTU1N30.vNY7bIMHN_r40vZh_TF_TxKFpy6M48VA7mJn_7F79L4`
       }});
       set({ data: response.data, loading: false },
         console.log(response.data));
@@ -40,7 +40,7 @@ const useStore = create((set, get) => ({
         const response = await axios.post(url, commentData, {
           headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QxMjM0Iiwicm9sZSI6IlVTRVIiLCJpZCI6NCwic2lndW5ndSI6MTAwLCJpYXQiOjE3MDY4MzY2MTUsImV4cCI6MTcwNzI2ODYxNX0.5jG0Us4yKDSTowVlebRwW1iP03py78fdSdPCcRTzKnk`
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QxMjM0Iiwicm9sZSI6IlVTRVIiLCJpZCI6NCwic2lndW5ndSI6MTAwLCJpYXQiOjE3MDY4NTc1NTcsImV4cCI6MTcwNzI4OTU1N30.vNY7bIMHN_r40vZh_TF_TxKFpy6M48VA7mJn_7F79L4`
           }
         });
         console.log('댓글 추가됨:', response.data);
@@ -52,7 +52,32 @@ const useStore = create((set, get) => ({
   alert(error.response?.data?.message || '댓글 추가 중 오류가 발생했습니다.');
         set({ error, loading: false });
       }
-    }
+    },
+
+    sendPostBulletin: async (Bulletin) => {
+      const url = `http://192.168.30.206:8080/api/helps`;
+      Bulletin.forEach((key, value) => {
+        console.log(key, value);
+        console.log(typeof(key))
+      });
+      set({ loading: true });
+      try {
+        const response = await axios.post(url, Bulletin, {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3QxMjM0Iiwicm9sZSI6IlVTRVIiLCJpZCI6NCwic2lndW5ndSI6MTAwLCJpYXQiOjE3MDY4NTc1NTcsImV4cCI6MTcwNzI4OTU1N30.vNY7bIMHN_r40vZh_TF_TxKFpy6M48VA7mJn_7F79L4`
+          }
+        });
+        console.log('게시글 추가됨:', response.data);
+        set({ loading: false });
+        // 여기에서 추가적인 상태 업데이트나 액션을 취할 수 있습니다.
+        // 예: 새로운 댓글 데이터를 상태에 추가하는 등
+      } catch (error) {
+        console.error('게시글 추가 실패:', error.response?.data || error);
+  alert(error.response?.data?.message || '게시글 추가 중 오류가 발생했습니다.');
+        set({ error, loading: false });
+      }
+    },
+
 }));
 
 export default useStore;
