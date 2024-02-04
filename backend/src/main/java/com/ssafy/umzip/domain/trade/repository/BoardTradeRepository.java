4c256eee23a10de90414e910a8c16d90a0bacb2d
김동현 <smdong972000@naver.com>
2024-02-04 17:50:09 +0900

    Refactor. 해당 사용자의 판매 물품 가져오기
    query문 수정
    
    Related-to: #31

--- backend/src/main/java/com/ssafy/umzip/domain/trade/repository/BoardTradeRepository.java
+++ backend/src/main/java/com/ssafy/umzip/domain/trade/repository/BoardTradeRepository.java
@@ -17,10 +17,14 @@ public interface BoardTradeRepository extends JpaRepository<BoardTrade, Long> {
             "or (b.codeSmall.id = :codeSmallId and b.title like %:title% and b.isDirect = false)")
     Slice<BoardTrade> findBoardTradeList(@Param("title") String title, @Param("sigungu") int sigungu,
                                          @Param("codeSmallId") Long codeSmallId, Pageable pageable);
-//
+
 //    @Query("select new com.ssafy.umzip.domain.trade.dto.ListDto(b.boardId, b.boardTradeImageList, b.title, b.address, b.price, b.readCnt, b.codeSmall.getId(), b.codeSmall.getName()) " +
 //            " from BoardTrade b LEFT JOIN BoardTradeImageList bt ON b.id = bt.boardTrade.getId() ")
 //    Slice<ListDto> findTest();
 
+    @Query("select b from BoardTrade b " +
+            "where b.id not in ( select ba.boardTrade.id from BoardTradeActive ba ) " +
+            "and b.member.id = :memberId ")
     Page<BoardTrade> findAllByMemberId(Long memberId, Pageable pageable);
 }
+
