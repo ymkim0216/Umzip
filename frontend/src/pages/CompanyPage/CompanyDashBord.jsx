1745e6f6c4b79489f61428c1fee89e207d083202
unknown <chiru7080@daum.net>
2024-02-06 15:59:25 +0900

    Feat: 업체대시보드 페이지 파일생성

---
+++ frontend/src/pages/CompanyPage/CompanyDashBord.jsx
@@ -0,0 +1,18 @@
+import CompanyMain from '../../components/CompanyMainPage/CompanyMain';
+import Header from "../../components/Header";
+import Chat from "../../components/Chat/Chat";
+
+
+
+
+function Help() {
+        return (
+        <>
+            <Header />
+            <h1>업체 대시보드</h1>
+            <CompanyMain />
+            <Chat />
+        </>
+        );
+}
+export default Help;
