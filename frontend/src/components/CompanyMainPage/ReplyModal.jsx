2de819718cc410e3112c48a6292bbcc36b0c7e2a
장현욱 <chiru7080@daum.net>
2024-02-09 23:44:59 +0900

    Feat: 업체 재견적 기능추가

--- frontend/src/components/CompanyMainPage/ReplyModal.jsx
+++ frontend/src/components/CompanyMainPage/ReplyModal.jsx
@@ -8,7 +8,7 @@ function ReplyModal({ role, onClose, mappingId, price }) {
 
     const handleSubmit = async (event) => {
       event.preventDefault();
-      const price = parseFloat(event.target.estimate.value); // input으로부터의 값은 문자열이므로, 숫자로 변환
+      const price = operPrice; // input으로부터의 값은 문자열이므로, 숫자로 변환
       const detail = event.target.message.value;
   
       await submitReplyStore(role, mappingId, price, detail); // Zustand store의 함수 호출
@@ -16,14 +16,14 @@ function ReplyModal({ role, onClose, mappingId, price }) {
     };
 
   return (
-    <div className='modalContnet'>
+    <div className='modalContent'>
     <div style={{ /* 모달 스타일 */ }}>
       <h2>견적 제안</h2>
       <form onSubmit={handleSubmit}>
         {/* 견적 가격과 메시지 입력 필드 */}
         <div>
           <label>견적 가격:</label>
-          <input name="estimate" type="number" value={operPrice} onChange={(e) => setPrice(e.target.value)} required />      
+          <input name="estimate" type="number" value={operPrice} onChange={(e) => setPrice(parseFloat(e.target.value))} required />      
         </div>
         <div>
           <label>메시지:</label>
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -4,7 +4,7 @@ import useAuthStore from '../store/store';
 const api = axios.create({
   // http://172.30.1.68:8080/api 윤민이 API => 윤민이 게인노트북
   // http://192.168.30.125:8080/api 윤민이 API => 싸피노트북
-  // baseURL: 'http://192.168.30.206:8080/api' 동현
+  // http://192.168.30.206:8080/api 동현
   // http://192.168.30.145:8080/api 민수
   // https://i10e108.p.ssafy.io/api 빌드 주소
   baseURL: 'https://i10e108.p.ssafy.io/api'
