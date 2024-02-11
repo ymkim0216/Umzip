4b1917ff7ce5cf39420cffa4a2e8d5b8d5b6daf3
mins00oo <alstn7223@gmail.com>
2024-02-11 15:42:31 +0900

    Refactor: 채팅방 접속 시 메시지 불러오기 api 수정
    채팅방을 불러올 때 상대방이 업체 회원이라면 업체의 이름과 사진 등 정보를 불러와야 하기 때문에 role으로 분기처리를 한 번 한다.
    해당하면 getMember를 통해 member 객체를 가져온다.
    
    Related-to: #39

--- backend/src/main/java/com/ssafy/umzip/domain/chat/service/ChatServiceImpl.java
+++ backend/src/main/java/com/ssafy/umzip/domain/chat/service/ChatServiceImpl.java
@@ -13,6 +13,7 @@ import com.ssafy.umzip.domain.member.entity.Member;
 import com.ssafy.umzip.domain.member.repository.MemberRepository;
 import com.ssafy.umzip.domain.trade.entity.BoardTradeImage;
 import com.ssafy.umzip.domain.trade.repository.BoardTradeImageRepository;
+import com.ssafy.umzip.global.common.Role;
 import com.ssafy.umzip.global.common.StatusCode;
 import com.ssafy.umzip.global.exception.BaseException;
 import lombok.RequiredArgsConstructor;
@@ -66,13 +67,22 @@ public class ChatServiceImpl implements ChatService {
     public ChatDetailResponseDto retrieveChatRoomMessages(Long chatRoomId, Long requestId, String role) {
         Long id = resolveMemberIdByRole(requestId, role);
         Member requester = findMemberById(id);
-        Member anotherMember = chatParticipantRepository.findByChatRoomAndMemberId(chatRoomId, id);
+        ChatParticipant cp = chatParticipantRepository.findByChatRoomAndMemberId(chatRoomId, id);
+        Member anotherMember;
+
+        if (cp.getRole().equals("DELIVER") || cp.getRole().equals("CLEAN")) {
+            anotherMember = companyRepository.findByMemberIdAndRole(cp.getId(), Role.valueOf(cp.getRole()))
+                    .orElseThrow(() -> new BaseException(StatusCode.NOT_EXIST_COMPANY)).getMember();
+        } else {
+            anotherMember = cp.getMember();
+        }
 
         List<ChatMessage> chatMessages = chatMessageRepository.findAllByChatRoomId(chatRoomId);
 
         List<ChatMessageResponseDto> responseDto = chatMessages.stream()
                 .map(message -> {
                     Member userInfo = message.getSenderId().equals(requestId.toString()) ? requester : anotherMember;
+
                     return new ChatMessageResponseDto(
                             message.getContent(),
                             requestId,
