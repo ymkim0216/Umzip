5190be42782f607f14d68be8c00b7600f1c50112
Leejangha <jangha0423@gmail.com>
2024-02-14 14:55:24 +0900

    Design: 중고 페이지 CSS 수정

--- frontend/src/components/Trade/TradeItemDetail.jsx
+++ frontend/src/components/Trade/TradeItemDetail.jsx
@@ -6,11 +6,12 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper/modules';
+import StarRating from '../Recommend/StarRating';
 
 import classes from './TradeItemDetail.module.css';
 import { AnimatePresence, motion } from 'framer-motion';
 import useAuthStore from '../../store/store';
-import { Client } from "@stomp/stompjs";
+import { Client } from '@stomp/stompjs';
 import { api } from '../../services/api';
 function ReportModal({ onClose }) {
   return (
@@ -63,13 +64,13 @@ function TradeItemDetail({ trade }) {
   // console.log(trade);
   const submit = useSubmit();
 
-  const [chatRoom,setChatRoom] =useState(null)
-  const [isModalOpen,setIsModalOpen] =useState(false)
+  const [chatRoom, setChatRoom] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const stompClientRef = useRef(null);
   const chatContainerRef = useRef();
-  const [userId,setUserId] = useState(null)
-  const [talkHistory ,setTalkHistory] = useState([])
-  const [userinput, setUserInput] = useState("");
+  const [userId, setUserId] = useState(null);
+  const [talkHistory, setTalkHistory] = useState([]);
+  const [userinput, setUserInput] = useState('');
   const createTime = new Date(trade.createDt);
   const now = new Date();
   const diffTime = Math.abs(now - createTime);
@@ -106,7 +107,9 @@ function TradeItemDetail({ trade }) {
   };
   const handleSale = async () => {
     try {
-      const response = await api.post('/trade-items/completed-sales', {boardId: trade.boardId} );
+      const response = await api.post('/trade-items/completed-sales', {
+        boardId: trade.boardId,
+      });
       navigate(-1);
       console.log(response);
     } catch (error) {
@@ -117,30 +120,29 @@ function TradeItemDetail({ trade }) {
   //========================
   const handleinput = (event) => {
     setUserInput(event.target.value);
-};
+  };
   const scrollToBottom = () => {
     // 스크롤 위치를 항상 맨 아래로 조절
     if (chatContainerRef.current) {
-      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
     }
   };
   useEffect(() => {
     scrollToBottom();
   }, [isModalOpen]);
 
-  const toggleModal = async ({tradeId,memberId}) => {
-    console.log(tradeId,memberId)
-    setIsModalOpen(true)
-    const res = await MakeRoom({tradeId,memberId})
+  const toggleModal = async ({ tradeId, memberId }) => {
+    console.log(tradeId, memberId);
+    setIsModalOpen(true);
+    const res = await MakeRoom({ tradeId, memberId });
     const stompClient = socket(res);
     stompClientRef.current = stompClient;
-    stompClient.onConnect(
-      stompClient.activate()
-    )
-  }
+    stompClient.onConnect(stompClient.activate());
+  };
   const socket = (res) => {
     const { token } = useAuthStore.getState();
-    console.log(res)
+    console.log(res);
     const client = new Client({
       brokerURL: `wss://i10e108.p.ssafy.io/ws?accessToken=${token}`,
       // brokerURL: `ws://192.168.30.145:8080/ws?accessToken=${token}`,
@@ -149,16 +151,15 @@ function TradeItemDetail({ trade }) {
         console.log('Connected: ' + frame);
 
         client.subscribe(`/topic/user/${token}`, (message) => {
-
           setUserId((prev) => {
-            const updatedHistory = message.body
+            const updatedHistory = message.body;
             // console.log(message.body);
             return updatedHistory;
-          })
+          });
         });
 
         client.subscribe(`/topic/chatroom/${res}`, (message) => {
-          console.log(res)
+          console.log(res);
           console.log('Received message: ' + message.body);
           // console.log(talkHistory)
           showReceivedMessage(message.body);
@@ -168,14 +169,14 @@ function TradeItemDetail({ trade }) {
       onStompError: (frame) => {
         console.error('Broker reported error: ' + frame.headers['message']);
         console.error('Additional details: ' + frame.body);
-      }
+      },
     });
 
     return client;
   };
   const showReceivedMessage = (message) => {
     try {
-      console.log(message)
+      console.log(message);
 
       const jsonData = JSON.parse(message);
       console.log(jsonData);
@@ -184,9 +185,7 @@ function TradeItemDetail({ trade }) {
         const updatedHistory = [...prevTalkHistory, jsonData];
         // console.log(updatedHistory);
         return updatedHistory;
-
-      })
-
+      });
     } catch (error) {
       console.error('Error parsing received message:', error);
     }
@@ -196,34 +195,32 @@ function TradeItemDetail({ trade }) {
     // userinput을 사용하도록 수정
     const { token } = useAuthStore.getState();
     if (userinput && stompClientRef.current.active) {
-      console.log(tradeId,chatRoom)
+      console.log(tradeId, chatRoom);
       stompClientRef.current.publish({
         destination: `/app/chat/${chatRoom}`,
         body: JSON.stringify({
           content: userinput,
           type: 'TALK',
-          tradeId:tradeId,
+          tradeId: tradeId,
         }),
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
     } else {
       console.error('Message is empty or stomp client is not connected.');
     }
   };
-  const MakeRoom = async ({tradeId,memberId}) => {
-
-
+  const MakeRoom = async ({ tradeId, memberId }) => {
     // console.log(companyId)
     try {
       const response = await api.post(
-        `/chat/trade/${tradeId}/${memberId}`,
+        `/chat/trade/${tradeId}/${memberId}`
         // 요청 바디를 올바른 위치에 추가
       );
-      setChatRoom(response.data.result)
+      setChatRoom(response.data.result);
       // console.log(response.data.result);
-      return response.data.result
+      return response.data.result;
     } catch (error) {
       console.error(error);
     }
@@ -231,12 +228,10 @@ function TradeItemDetail({ trade }) {
 
   const stopSocketCommunication = () => {
     if (stompClientRef.current) {
-
       stompClientRef.current.deactivate();
-      console.log("연결X")
+      console.log('연결X');
     }
   };
-
 
   //=====================
 
@@ -244,9 +239,16 @@ function TradeItemDetail({ trade }) {
     <>
       <AnimatePresence>
         {isModalOpen && (
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} onClick={() => { setIsModalOpen(false); setTalkHistory([]), setUserInput(""), stopSocketCommunication() }}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={() => {
+              setIsModalOpen(false);
+              setTalkHistory([]), setUserInput(''), stopSocketCommunication();
+            }}
             style={{
-              zIndex: "99",
+              zIndex: '99',
               position: 'fixed',
               top: 0,
               left: 0,
@@ -256,61 +258,105 @@ function TradeItemDetail({ trade }) {
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center',
-            }}>
-            <div style={{
-              display: "flex",
-              position: 'relative',
-              width: '40%',
-              height: "70%",
-              backgroundColor: 'white',
-              padding: '20px',
-              borderRadius: '8px',
-            }}>
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                position: 'relative',
+                width: '40%',
+                height: '70%',
+                backgroundColor: 'white',
+                padding: '20px',
+                borderRadius: '8px',
+              }}
+            >
               <div
                 onClick={(e) => e.stopPropagation()}
                 ref={chatContainerRef}
                 style={{
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  overflowY: "auto"
+                  width: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  overflowY: 'auto',
                 }}
               >
-                {userId && talkHistory && talkHistory.map((items, index) => (
-                  <div className='d-flex  flex-column'>
-                    <div className='' style={{ alignSelf: userId !== items.senderId ? "flex-start" : "flex-end", }}>
-                      {userId !== items.senderId ? <div className='d-flex align-items-center gap-1 justify-content-center'><img src={items.senderProfileImage} style={{ width: "2rem", height: "2rem" }} className='rounded-pill' />
-                        <p className='m-0'>{items.senderName}</p></div> : <div className='d-flex align-items-center gap-1 justify-content-center'><p className='m-0'>{items.senderName}</p><img src={items.senderProfileImage} style={{ width: "2rem", height: "2rem" }} className='rounded-pill' />
-                      </div>}
-                    </div>
-                    <div
-                      key={index}
-                      style={{
+                {userId &&
+                  talkHistory &&
+                  talkHistory.map((items, index) => (
+                    <div className="d-flex  flex-column">
+                      <div
+                        className=""
+                        style={{
+                          alignSelf:
+                            userId !== items.senderId
+                              ? 'flex-start'
+                              : 'flex-end',
+                        }}
+                      >
+                        {userId !== items.senderId ? (
+                          <div className="d-flex align-items-center gap-1 justify-content-center">
+                            <img
+                              src={items.senderProfileImage}
+                              style={{ width: '2rem', height: '2rem' }}
+                              className="rounded-pill"
+                            />
+                            <p className="m-0">{items.senderName}</p>
+                          </div>
+                        ) : (
+                          <div className="d-flex align-items-center gap-1 justify-content-center">
+                            <p className="m-0">{items.senderName}</p>
+                            <img
+                              src={items.senderProfileImage}
+                              style={{ width: '2rem', height: '2rem' }}
+                              className="rounded-pill"
+                            />
+                          </div>
+                        )}
+                      </div>
+                      <div
+                        key={index}
+                        style={{
+                          maxWidth: '70%',
+                          margin: '5px',
+                          padding: '10px',
+                          borderRadius: '10px',
+                          alignSelf:
+                            userId !== items.senderId
+                              ? 'flex-start'
+                              : 'flex-end',
+                          background:
+                            userId !== items.senderId ? '#e6e6e6' : '#4caf50',
 
-                        maxWidth: "70%",
-                        margin: "5px",
-                        padding: "10px",
-                        borderRadius: "10px",
-                        alignSelf: userId !== items.senderId ? "flex-start" : "flex-end",
-                        background: userId !== items.senderId ? "#e6e6e6" : "#4caf50",
-
-                        color: userId !== items.senderId ? "#000" : "#fff",
-                      }}
-                    >
-                      {items.content}
+                          color: userId !== items.senderId ? '#000' : '#fff',
+                        }}
+                      >
+                        {items.content}
+                      </div>
                     </div>
-                  </div>
-                ))}
-                <div style={{ marginTop: "auto" }}>
-                  <form className='d-flex justify-content-around' onSubmit={(e) => { e.preventDefault(); sendMessage(trade.boardId); setUserInput(''); }}>
-                    <input value={userinput} className='col-10 border px-3 bg-white shadow-lg rounded-3' type='text' onChange={handleinput} />
-                    <button type="submit" className='btn btn-primary rounded-4'><img src='/Paper_Plane.png' /></button>
+                  ))}
+                <div style={{ marginTop: 'auto' }}>
+                  <form
+                    className="d-flex justify-content-around"
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      sendMessage(trade.boardId);
+                      setUserInput('');
+                    }}
+                  >
+                    <input
+                      value={userinput}
+                      className="col-10 border px-3 bg-white shadow-lg rounded-3"
+                      type="text"
+                      onChange={handleinput}
+                    />
+                    <button type="submit" className="btn btn-primary rounded-4">
+                      <img src="/Paper_Plane.png" />
+                    </button>
                   </form>
                 </div>
-
               </div>
             </div>
-
           </motion.div>
         )}
       </AnimatePresence>
@@ -336,8 +382,8 @@ function TradeItemDetail({ trade }) {
             </SwiperSlide>
           ))}
         </Swiper>
-        <div style={{ width: '85%', marginLeft: 'auto', marginRight: 'auto' }}>
-          <div className="d-flex flex-row p-2 justify-content-between m-1">
+        <div style={{ width: '83%', marginLeft: 'auto', marginRight: 'auto' }}>
+          <div className={classes.writer}>
             <div className="d-flex gap-2">
               <motion.img
                 onClick={handleClick}
@@ -348,24 +394,36 @@ function TradeItemDetail({ trade }) {
                 alt="랜덤 이미지"
               ></motion.img>
               <div className="text-start">
-                <p className="m-0">{trade.writerName}</p>
-                <p>{trade.writerAddress}</p>
+                <div className="m-0">{trade.writerName}</div>
+                <div>{trade.writerAddress}</div>
               </div>
             </div>
-            <div className="position-relative p-2">
-              <small className="form-text text-muted">{trade.writerRating}</small>
+            <div className="d-flex gap-2">
+              <img
+                src="/filled-star.png"
+                alt="star"
+                className={classes.star}
+                style={{ width: '30px', height: 'auto' }}
+              />
+              <small className="form-text text-muted">
+                {trade.writerRating}
+              </small>
             </div>
           </div>
         </div>
         <div className={classes.article}>
-          <h2 className={classes.title}>제목: {trade.title}</h2>
-          <div className={classes.price}>
-            <p>{trade.price}원</p>
+          <h2 className={classes.title}>{trade.title}</h2>
+          <div className={classes.detail}>
+            <p className={classes.price}>
+              {trade.price.toLocaleString('ko-KR')}원
+            </p>
             <p className={classes.date}>
               {diffHours < 24 ? `${diffHours} 시간 전` : `${yy}-${mm}-${dd}`}
             </p>
           </div>
-          <p>{trade.direct ? '직거래' : '택배배송'}</p>
+          <p className={classes.direct}>
+            {trade.direct ? '직거래' : '택배배송'}
+          </p>
           <div className={classes.content}>
             {trade.content}
             <p className={classes.view}>조회 {trade.readCnt}</p>
@@ -395,14 +453,24 @@ function TradeItemDetail({ trade }) {
               </menu>
             ) : (
               <menu className={classes.chat}>
-                <button onClick={()=>toggleModal({"tradeId": trade.boardId , "memberId" :trade.writerId })}>채팅</button>
+                <button
+                  onClick={() =>
+                    toggleModal({
+                      tradeId: trade.boardId,
+                      memberId: trade.writerId,
+                    })
+                  }
+                >
+                  채팅
+                </button>
                 {/* {isChatOpen && <Chat />} */}
               </menu>
             )}
           </div>
         </div>
       </article>
-    </>);
+    </>
+  );
 }
 
 export default TradeItemDetail;
