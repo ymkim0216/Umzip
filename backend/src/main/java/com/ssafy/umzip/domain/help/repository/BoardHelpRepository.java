3e82dd3b0de23c42ca032b135706b744996cc7ee
김동현 <smdong972000@naver.com>
2024-02-14 10:09:13 +0900

    Fix. 수정 내역
    1. 프로필: 도움 구인, 도움 내역
    2. BoardTradeActive에 BaseTimeEntity 추가
    
    Related by: #56

--- backend/src/main/java/com/ssafy/umzip/domain/help/repository/BoardHelpRepository.java
+++ backend/src/main/java/com/ssafy/umzip/domain/help/repository/BoardHelpRepository.java
@@ -10,11 +10,11 @@ public interface BoardHelpRepository extends JpaRepository<BoardHelp, Long> {
 
     @Query("select b from BoardHelp b " +
             "where b.member.id = :memberId " +
-            "and (b.codeSmall.id = 401L or b.codeSmall.id = 402L ) ")
+            "and (b.codeSmall.id = 401L or b.codeSmall.id = 403L ) ")
     Page<BoardHelp> findAllByMemberIdMe(Long memberId, Pageable pageable);
 
     @Query("select b from BoardHelp b " +
             "where b.member.id = :memberId " +
-            "and b.codeSmall.id = 403L ")
+            "and b.codeSmall.id = 402L ")
     Page<BoardHelp> findAllByMemberIdYou(Long memberId, Pageable pageable);
 }
